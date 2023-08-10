2f8cb1aa737160b16d8f4ee156a1b5d3d681ff56
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-08-10 12:59:40 +0600

    cobined frontend, backend project structure created

---
+++ Practice-02(Combined)/myApp/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const router = require('./src/routes/api');
+
+const app = express();
+
+//security middleware
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+
+//import mongoose
+const mongoose = require('mongoose');
+
+
+//security middleware implementation
+app.use(helmet());
+app.use(cors());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+
+//parse json data
+app.use(express.json());
+
+
+//Rate Limiter Implementation
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,    // 1 minute
+    max: 10,
+    message: 'Too many request from this IP. Please try again later'
+})
+app.use(limiter);
+
+//database connection
+
+
+
+//Frontend connection
+app.use(express.static('client/dist'));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+})
+
+
+
+//routing
+app.use('/api/v1', router);
+
+module.exports = app;
---
+++ Practice-02(Combined)/myApp/index.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(8000, () => {
+    console.log("Server is running port on  8000");
+})
