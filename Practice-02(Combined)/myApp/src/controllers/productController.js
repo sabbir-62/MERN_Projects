e9c92fce668edd932b93735dba274f62d19fb46e
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-08-12 19:40:59 +0600

    update, delete operation created

--- Practice-02(Combined)/myApp/src/controllers/productController.js
+++ Practice-02(Combined)/myApp/src/controllers/productController.js
@@ -1,6 +1,7 @@
 const ProductModel = require('../models/ProductsModel');
 
 
+// Create Product
 exports.createProduct = (req, res) => {
     let reqBody = req.body;
     ProductModel.create(reqBody, (err, data) => {
@@ -19,11 +20,57 @@ exports.createProduct = (req, res) => {
     })
 }
 
+
+//Read Product
 exports.readProduct = (req, res) => {
     let query = {};
     let projection = {};
    
     ProductModel.find(query, projection, (err, data) => {
+        if(err){
+            res.status(400).json({
+                status: "Fail",
+                data: err
+            })
+        }
+        else{
+            res.status(200).json({
+                status: "Success",
+                data: data
+            })
+        }
+    })
+}
+
+
+//Update Product
+exports.updateProduct = (req, res) => {
+    let id = req.param.id;
+    let query = {_id: id};
+   
+    ProductModel.updateOne(query, (err, data) => {
+        if(err){
+            res.status(400).json({
+                status: "Fail",
+                data: err
+            })
+        }
+        else{
+            res.status(200).json({
+                status: "Success",
+                data: data
+            })
+        }
+    })
+}
+
+
+//Delete Product
+exports.deleteProduct = (req, res) => {
+    let id = req.param.id;
+    let query = {_id: id};
+   
+    ProductModel.remove(query, (err, data) => {
         if(err){
             res.status(400).json({
                 status: "Fail",
