9f872566b06c6991be56b32106889fafc4e7ee32
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-08-13 18:43:41 +0600

    api endpoints created

--- Practice-02(Combined)/myApp/src/routes/api.js
+++ Practice-02(Combined)/myApp/src/routes/api.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const productController = require('../controllers/productController');
 
 //api endpoints
+router.post("/createProduct", productController.createProduct);
+
+router.get("/readProduct", productController.readProduct);
+
+router.post("/updateProduct/:id", productController.updateProduct);
+
+router.get("/deleteProduct/:id", productController.deleteProduct);
+
 
 
 module.exports = router;
