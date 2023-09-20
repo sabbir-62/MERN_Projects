41123d18e39fb2c405b5716771eaa2a1472e6687
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-09-20 21:50:40 +0600

    navbar added

---
+++ Alumni/client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from './conponents/navbar/Navbar';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
