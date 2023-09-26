0b34173912084d25da4336737d8bbf29d67b7b28
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-09-26 18:08:26 +0600

    user registration form created

--- Alumni/client/src/components/navbar/Navbar.jsx
+++ Alumni/client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import { NavLink} from 'react-router-dom'
+import Logo from '../../assets/pictures/logo.png'
 
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" to="/">Navbar</NavLink>
+                    <NavLink className="navbar-brand" to="/">
+                        <img className='image-thumbnail' style={{width:"50px"}} src={Logo} alt="logo" />
+                    </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
--- Alumni/client/src/pages/registrationPage/RegistrationPage.jsx
+++ Alumni/client/src/pages/registrationPage/RegistrationPage.jsx
@@ -1,9 +1,43 @@
-
+import './registration.css'
+import { NavLink } from 'react-router-dom';
 
 const RegistrationPage = () => {
+    const handleSubmit = () => {
+
+    }
     return (
-        <div>
-            This is Registration page
+        <div className="registration">
+            <div className="container">
+                <form className="form" action="" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <lebel htmlFor="name"></lebel>
+                        <input type="text" className='input-field'  placeholder="Enter Your Name"/>
+                    </div>
+                    <div className="form-group">
+                        <lebel htmlFor="email"></lebel>
+                        <input type="text" className='input-field' placeholder="Enter Your Email"/>
+                    </div>
+                    <div className="form-group">
+                        <lebel htmlFor="phone"></lebel>
+                        <input type="tel" className='input-field' placeholder="Enter Your Mobile Number"/>
+                    </div>
+                    <div className="form-group">
+                        <lebel htmlFor="password"></lebel>
+                        <input type="text" className='input-field' placeholder="Enter Your Password"/>
+                    </div>
+                    <div className="form-group">
+                        <lebel htmlFor="cpassword"></lebel>
+                        <input type="text" className='input-field' placeholder="Enter Your Confirm Password"/>
+                    </div>
+                    <div className="form-group">
+                        <div className="button">
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <NavLink className="nav-link" to="/login">I am already register</NavLink>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            
         </div>
     );
 };
