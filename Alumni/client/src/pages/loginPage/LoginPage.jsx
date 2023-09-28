36546a5ff2eb05fcc6714b1e980c4d3d32806841
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-09-28 18:05:03 +0600

    login form created

--- Alumni/client/src/pages/loginPage/LoginPage.jsx
+++ Alumni/client/src/pages/loginPage/LoginPage.jsx
@@ -1,9 +1,38 @@
-
+import './login.css'
+import { NavLink } from 'react-router-dom';
 
 const LoginPage = () => {
+    const handleSubmit = () => {
+
+    }
     return (
-        <div>
-            This is Login page
+        <div className="registration">
+            <div className="container">
+                <form className="form" action="" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <lebel htmlFor="email"></lebel>
+                        <input type="text" className='input-field' placeholder="Enter Your Email"/>
+                    </div>
+                    <div className="form-group">
+                        <lebel htmlFor="password"></lebel>
+                        <input type="text" className='input-field' placeholder="Enter Your Password"/>
+                    </div>
+                    <div className="form-group">
+                        <lebel htmlFor="cpassword"></lebel>
+                        <input type="text" className='input-field' placeholder="Enter Your Confirm Password"/>
+                    </div>
+                    <div className="form-group">
+                        <div className="button">
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <div className='new-user'>
+                                <span>Not a member?</span>
+                                <NavLink className="nav-link" to="/registration">Registration</NavLink>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            
         </div>
     );
 };
